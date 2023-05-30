3dcd6be12a36a247dc238871288cde3731610c18
Hamad <hamad@Hamads-MBP.lan>
2023-05-30 12:39:47 -0400

    Removal of redundancies within taskManager and Database

--- cli.py
+++ cli.py
@@ -1,9 +1,8 @@
 import os
 
 class CLI:
-    def __init__(self, master_db, updates_db):
-        self.master_db = master_db
-        self.updates_db = updates_db
+    def __init__(self, task_manager):
+        self.task_manager = task_manager
 
     def run_cli(self):
         while True:
@@ -17,88 +16,32 @@ class CLI:
 
             if choice == '1':
                 task_name = input("\nEnter the name of the new Master Task: ")
-                self.master_db.create_task(task_name)
+                self.task_manager.create_master_task(task_name)
 
             elif choice == '2':
-                self.list_master_tasks()
+                self.task_manager.list_master_tasks()
 
             elif choice == '3':
-                task_id, _ = self.select_master_task()  # we don't care about task_name here
-                update_text = input("\nEnter your task update (max 300 chars): ")
-                self.updates_db.add_update(task_id, update_text)
+                task_id, _ = self.task_manager.select_master_task()
+                if task_id:
+                    update_text = input("\nEnter your task update (max 300 chars): ")
+                    self.task_manager.add_task_update(task_id, update_text)
 
             elif choice == '4':
-                task_id, task_name = self.select_master_task()
-                self.get_task_updates(task_id, task_name)
+                task_id, task_name = self.task_manager.select_master_task()
+                if task_id:
+                    self.task_manager.get_task_updates(task_id, task_name)
 
             elif choice == '5':
                 print("\nExiting...\n")
                 break
 
             else:
-                print("\nInvalid choice. Please enter a number between 1 and 4.\n")
+                print("\nInvalid choice. Please enter a number between 1 and 5.\n")
 
-    def list_master_tasks(self):
-        print("\nMaster Task List")
-        print("================")
-        tasks = self.master_db.list_tasks()
-        for i, task in enumerate(tasks):
-            print(f"{i+1}. {task[1]}")
-        print("================\n")
-
-    def select_master_task(self):
-        self.list_master_tasks()
-        task_index = int(input("Please enter the number of the task you want to select: "))
-        tasks = self.master_db.list_tasks()
-        if 1 <= task_index <= len(tasks):
-            selected_task = tasks[task_index - 1]
-            return selected_task[0], selected_task[1]  # returns both task_id and task_name
-        else:
-            print("\nInvalid selection.")
-            return None, None
-
-    def get_task_updates(self, task_id, task_name):
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-        updates = self.updates_db.get_updates(task_id)
-        # Group updates by date
-        updates_by_date = {}
-        for update in updates:
-            date, time, text = update
-            if date in updates_by_date:
-                updates_by_date[date].append((time, text))
-            else:
-                updates_by_date[date] = [(time, text)]
-
-        # Display updates
-        print(f"\nTask Updates for {task_name}\n------------")
-        for date, updates_on_date in updates_by_date.items():
-            print(f"\nDate: {date}")
-            print("----------------")
-            for update in updates_on_date:
-                time, text = update
-                print(f"{time} -- Update: {text}\n")
-        print("====================================\n")
-        
-        while True:
-            print("Options:")
-            print("1. Add new update")
-            print("2. Go back to main menu")
-            option = input("Choose your option: ")
-
-            if option == '1':
-                update_text = input("Enter your task update (max 300 chars): ")
-                self.updates_db.add_update(task_id, update_text)
-                # Refresh task updates view after adding new update
-                self.get_task_updates(task_id, task_name)
-            elif option == '2':
-                break
-            else:
-                print("Invalid option, try again.")
 
 if __name__ == "__main__":
-    from database import MasterTaskDB, TaskUpdateDB
-    master_db = MasterTaskDB('task.db')
-    updates_db = TaskUpdateDB('task.db')
-    cli = CLI(master_db, updates_db)
+    from taskManager import TaskManager
+    task_manager = TaskManager('task.db')
+    cli = CLI(task_manager)
     cli.run_cli()
--- taskManager.py
+++ taskManager.py
@@ -1,77 +1,43 @@
-import sqlite3
-from datetime import datetime
 import os
+from database import MasterTaskDB, TaskUpdateDB
 
 class TaskManager:
-    def __init__(self):
-        self.conn = sqlite3.connect('task.db')
-        self.cursor = self.conn.cursor()
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS master_tasks(
-                task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                task_name TEXT NOT NULL
-            );
-        """)
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS task_updates(
-                update_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                task_id INTEGER,
-                update_date TEXT NOT NULL,
-                update_time TEXT NOT NULL,
-                update_text TEXT NOT NULL,
-                FOREIGN KEY (task_id) REFERENCES master_tasks (task_id)
-            );
-        """)
+    def __init__(self, db_name):
+        self.master_db = MasterTaskDB(db_name)
+        self.updates_db = TaskUpdateDB(db_name)
 
     def create_master_task(self, task_name):
-        self.cursor.execute("""
-            INSERT INTO master_tasks (task_name) VALUES (?);
-        """, (task_name,))
-        self.conn.commit()
+        self.master_db.create_task(task_name)
 
     def add_task_update(self, task_id, update_text):
-        date_str = datetime.now().strftime("%m-%d-%Y")
-        time_str = datetime.now().strftime("%H:%M:%S")
-        self.cursor.execute("""
-            INSERT INTO task_updates (task_id, update_date, update_time, update_text) 
-            VALUES (?, ?, ?, ?);
-        """, (task_id, date_str, time_str, update_text))
-        self.conn.commit()
+        self.updates_db.add_update(task_id, update_text)
 
     def list_master_tasks(self):
         print("\nMaster Task List")
         print("================")
-        self.cursor.execute("""
-            SELECT * FROM master_tasks;
-        """)
-        tasks = self.cursor.fetchall()
+        tasks = self.master_db.list_tasks()
         for i, task in enumerate(tasks):
             print(f"{i+1}. {task[1]}")
         print("================\n")
 
     def select_master_task(self):
         self.list_master_tasks()
-        task_index = int(input("Please enter the number of the task you want to select: "))
-        self.cursor.execute("""
-            SELECT * FROM master_tasks;
-        """)
-        tasks = self.cursor.fetchall()
-        if 1 <= task_index <= len(tasks):
-            selected_task = tasks[task_index - 1]
-            return selected_task[0], selected_task[1]  # returns both task_id and task_name
-        else:
-            print("\nInvalid selection.")
-            return None, None
+        while True:
+            try:
+                task_index = int(input("Please enter the number of the task you want to select: "))
+                tasks = self.master_db.list_tasks()
+                if 1 <= task_index <= len(tasks):
+                    selected_task = tasks[task_index - 1]
+                    return selected_task[0], selected_task[1]
+                else:
+                    print("\nInvalid selection.")
+            except ValueError:
+                print("\nInvalid input, please enter a number.")
 
     def get_task_updates(self, task_id, task_name):
         os.system('cls' if os.name == 'nt' else 'clear')
 
-        self.cursor.execute("""
-            SELECT update_date, update_time, update_text FROM task_updates 
-            WHERE task_id = ? 
-            ORDER BY update_date ASC, update_time ASC;
-        """, (task_id,))
-        updates = self.cursor.fetchall()
+        updates = self.updates_db.get_updates(task_id)
         
         # Group updates by date
         updates_by_date = {}
@@ -100,44 +66,10 @@ class TaskManager:
 
             if option == '1':
                 update_text = input("Enter your task update (max 300 chars): ")
-                self.add_task_update(task_id, update_text)
+                self.updates_db.add_update(task_id, update_text)
                 # Refresh task updates view after adding new update
                 self.get_task_updates(task_id, task_name)
             elif option == '2':
                 break
             else:
                 print("Invalid option, try again.")
-
-
-def run_cli():
-    taskManager = TaskManager()
-    while True:
-        print("\nWelcome to the task manager, select from the menu below:\n")
-        print("1. Create Master Task")
-        print("2. Add Task Update")
-        print("3. View Task Updates")
-        print("4. Exit\n")
-        choice = input("Enter your choice: ")
-        
-        if choice == '1':
-            task_name = input("\nEnter the name of the new Master Task: ")
-            taskManager.create_master_task(task_name)
-
-        elif choice == '2':
-            task_id, _ = taskManager.select_master_task()  # we don't care about task_name here
-            update_text = input("\nEnter your task update (max 300 chars): ")
-            taskManager.add_task_update(task_id, update_text)
-
-        elif choice == '3':
-            task_id, task_name = taskManager.select_master_task()
-            taskManager.get_task_updates(task_id, task_name)
-
-        elif choice == '4':
-            print("\nExiting...\n")
-            break
-
-        else:
-            print("\nInvalid choice. Please enter a number between 1 and 4.\n")
-
-if __name__ == "__main__":
-    run_cli()
